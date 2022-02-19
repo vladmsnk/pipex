1d9f391126494f0998bbbe0bcf07497d1e69f431
vladmsnk <moiseenkov-v@mail.ru>
2022-02-19 23:47:46 +0300

    redo gnl

--- here_doc.c
+++ here_doc.c
@@ -1,13 +1,16 @@
 #include "pipex_bonus.h"
 
-void    here_doc_exec(int argc, char **argv, char **envp)
+void    here_doc_exec(int argc, char **argv, char **envp, int *fds1, int *fds2)
 {
+
     char	*line;
-	
 
-	while (1)
+	line = get_next_line(0);
+	dup2(fds1[1], STDOUT_FILENO);
+	close(fds1[1]);
+	while (ft_strncmp(line, argv[2], ft_strlen(argv[2])) == 0)
 	{
-		line = get_next_line(1);
-
+		write(1, line, ft_strlen(line));
+		free(line);
+		line = get_next_line(0);
 	}
-}
